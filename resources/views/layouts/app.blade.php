d15bbc11d6ff34d9c99338ce01bb62b5c5959b85
tomryus <sr.tomqq@gmail.com>
2019-04-04 17:28:13 +0700

    penambahan fitur view login dan halaman dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,128 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>SimpleAdminLTE 2 | Blank Page</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href={{asset('bootstrap/css/bootstrap.min.css')}}>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href={{asset("fonts/font-awesome.min.css")}}>
+    <!-- Ionicons -->
+    <link rel="stylesheet" href={{asset("fonts/ionicons.min.css")}}>
+    <!-- Theme style -->
+    <link rel="stylesheet" href={{asset("dist/css/AdminLTE.min.css")}}>
+    <link rel="stylesheet" href={{asset("dist/css/skins/_all-skins.min.css")}}>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+<body class="hold-transition skin-purple sidebar-mini">
+    <!-- Site wrapper -->
+    <div class="wrapper">
+
+    <header class="main-header">
+        <!-- Logo -->
+        <a href="#" class="logo">
+        <span class="logo-mini"><b>A</b>LT</span>
+        <span class="logo-lg">Laravel Blog<b>Admin</b>LTE</span>
+        </a>
+        <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </a>
+
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+            <li class="dropdown user user-menu">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                <span class="hidden-xs">{{Auth::user()->name}}</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
+                <ul class="dropdown-menu">
+                <li class="user-footer">
+                    <div class="pull-right">
+                        <a class="btn btn-default btn-flat" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                        {{ __('Sign out') }}
+                        </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    </div>
+                </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+            </li>
+            </ul>
+        </div>
         </nav>
+    </header>
 
-        <main class="py-4">
+
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+            <img src={{asset("dist/img/user2-160x160.jpg")}} class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+            <p>{{Auth::user()->name}}</p>
+            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+            <li class="treeview">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                <li><a href="#"><i class="fa fa-pencil"></i>Artikel</a></li>
+            </li>
+        </ul>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+        <h1>
+        @yield('page-title')
+        </h1>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
             @yield('content')
-        </main>
+        </section>
     </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b><a href="#">Laravel Blog</a></b>
+    </div>
+    <strong> <a href="#">Laravel Blog</a></strong>
+  </footer>
+<script src={{asset('plugins/jQuery/jquery-2.2.3.min.js')}}></script>
+<!-- Bootstrap 3.3.6 -->
+<script src={{asset("bootstrap/js/bootstrap.min.js")}}></script>
+<!-- SlimScroll -->
+<script src={{asset("plugins/slimScroll/jquery.slimscroll.min.js")}}></script>
+<!-- FastClick -->
+<script src={{asset("plugins/fastclick/fastclick.js")}}></script>
+<!-- AdminLTE App -->
+<script src={{asset("dist/js/app.min.js")}}></script>
+<!-- AdminLTE for demo purposes -->
+<script src={{asset("dist/js/demo.js")}}></script>
 </body>
 </html>
